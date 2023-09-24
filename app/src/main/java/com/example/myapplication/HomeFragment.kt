0a96e3060be3f3b2aaadb0200b62415fe5530a18
nech1234 <145868124+nech1234@users.noreply.github.com>
2023-09-24 15:21:17 +0900

    Initial commit

---
+++ app/src/main/java/com/example/myapplication/HomeFragment.kt
@@ -0,0 +1,33 @@
+package com.example.myapplication
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import com.example.myapplication.databinding.FragmentHomeBinding
+
+
+class HomeFragment : Fragment() {
+
+    lateinit var binding : FragmentHomeBinding
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+    }
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        binding = FragmentHomeBinding.inflate(inflater, container, false)
+
+        binding.homeAlbumImgIv1.setOnClickListener {
+            (context as MainActivity)
+                .supportFragmentManager.beginTransaction()
+                .replace(R.id.main_frm, AlbumFragment()).commitAllowingStateLoss()
+        }
+
+        return binding.root
+    }
+}
---
+++ app/src/main/java/com/example/myapplication/Song.kt
@@ -0,0 +1,12 @@
+package com.example.myapplication
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+data class Song(
+    val title : String = "",
+    val singer : String = ""
+//    val startTime : String = "",
+//    val endTime : String = "",
+//    val isplaying : Boolean = false
+)
