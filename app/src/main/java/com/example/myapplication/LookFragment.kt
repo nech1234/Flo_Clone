d8d7308bd4afe3a1129a6834b98d44763ca4df69
nech1234 <145868124+nech1234@users.noreply.github.com>
2023-10-12 15:50:35 +0900

    5주차 작업 1

--- app/src/main/java/com/example/myapplication/LookFragment.kt
+++ app/src/main/java/com/example/myapplication/LookFragment.kt
@@ -14,6 +14,8 @@ class LookFragment : Fragment() {
         super.onCreate(savedInstanceState)
     }
 
+    // 4주차 작업 
+
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
